243c55811106b73063cbf852b2523186311592d6
Sergey <62072065+Wildamager@users.noreply.github.com>
2022-03-03 00:16:20 +0300

    save_button_popup

--- music-script.js
+++ music-script.js
@@ -1,6 +1,6 @@
 var port = chrome.runtime.connect({name: "knockknock"});
 port.postMessage({joke: "Lets working"});
-port.onMessage.addListener(function(request,senger,sendResponse) {
+port.onMessage.addListener(function(request) {
     let info_of_music=request.done
     let url_of_image=request.info
     document.getElementById("name_of_track").innerHTML = info_of_music[1]
